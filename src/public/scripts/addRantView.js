094e672928ea8b8af72143ad7152425da2ad9963
Todd H. Gardner <todd@wocketware.com>
2017-01-14 17:18:54 -0600

    add storage bug

--- src/public/scripts/addRantView.js
+++ src/public/scripts/addRantView.js
@@ -1,7 +1,22 @@
 var AddRantView = Backbone.View.extend({
 
   events: {
-    submit: 'onSubmit'
+    'submit': 'onSubmit',
+    'keyup textarea': 'onChange'
+  },
+
+  initialize: function() {
+    var text = localStorage.getItem('next-rant');
+    if (text) {
+      this.$('textarea').val(text);
+    }
+  },
+
+  onChange: function(evt) {
+    var text = (evt.target || {}).value;
+    if (text) {
+      localStorage.setItem('next-rant', text);
+    }
   },
 
   onSubmit: function(evt) {
